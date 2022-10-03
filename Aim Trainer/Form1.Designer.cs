28c075c1a8f651d52c6e7b1322c15b76bd32bae5
eldarkaaa <112446396+eldarkaaa@users.noreply.github.com>
2022-10-03 17:00:54 +0300

    Initial commit

---
+++ Aim Trainer/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Aim_Trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.score = new System.Windows.Forms.Label();
+            this.roundButton1 = new RoundButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.score.Location = new System.Drawing.Point(672, 532);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(61, 20);
+            this.score.TabIndex = 2;
+            this.score.Text = "Score:";
+            // 
+            // roundButton1
+            // 
+            this.roundButton1.BackgroundImage = global::Aim_Trainer.Resource1.Target;
+            this.roundButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.roundButton1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.roundButton1.Location = new System.Drawing.Point(142, 36);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.Size = new System.Drawing.Size(100, 100);
+            this.roundButton1.TabIndex = 1;
+            this.roundButton1.UseVisualStyleBackColor = true;
+            this.roundButton1.Click += new System.EventHandler(this.roundButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = global::Aim_Trainer.Resource1.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.roundButton1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private RoundButton roundButton1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Aim Trainer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aim_Trainer
+{
+
+    public partial class Form1 : Form
+    {
+        private void TrueSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\eldar\source\repos\Aim Trainer\sound\hit.wav"); //Звук попадания
+            simpleSound.Play();
+        }
+        int _score = 0;
+        public Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void roundButton1_Click(object sender, EventArgs e)
+        {
+            roundButton1.Location = new Point(rand.Next(0, 650), rand.Next(0, 450));
+            AddScore(10);
+            TrueSound();
+
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void AddScore(int a)
+        {
+            _score += a;
+            score.Text = Convert.ToString("Score:" + _score);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            AddScore(-5);
+        }
+    }
+}
